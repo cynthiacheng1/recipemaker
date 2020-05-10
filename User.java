c7abc168b832accb546cbeb276b079cef4c6cabd
Cynthia <admin@Cynthias-MBP.fios-router.home>
2020-05-10 14:12:26 -0400

    userupdate

---
+++ User.java
@@ -0,0 +1,17 @@
+public User{
+  byte[] inventory;
+  String usename;
+  String password;
+  String date;
+
+  pubilc User(){
+    inventory
+  }
+
+  public User(byte[] currInventory, String user, String pass, String dat){
+    inventory = currInventory;
+    username = user;
+    password = pass;
+    date = dat;
+  }
+}
